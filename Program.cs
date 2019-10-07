e8d007c148bfe1e01ace472521c89a91778ab55b
Maggie Johnson <mmjohn09@icloud.com>
2019-10-07 15:11:51 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>(){
+                "Mercury", "Mars"};
+
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            List<string> lastThreePlanets = new List<string>(){
+                "Uranus", "Neptune", "Pluto"
+            };
+
+            planetList.AddRange(lastThreePlanets);
+
+            planetList.Insert(1, "Venus");
+            planetList.Insert(2, "Earth");
+
+            planetList.Remove("Pluto");
+
+            planetList.ForEach(planet => Console.WriteLine(planet));
+
+            List<string> rockyPlanets = planetList.GetRange(0, 4);
+            Console.WriteLine("---Rocky Planets---");
+            rockyPlanets.ForEach(planet => Console.WriteLine(planet));
+
+        }
+    }
+}
